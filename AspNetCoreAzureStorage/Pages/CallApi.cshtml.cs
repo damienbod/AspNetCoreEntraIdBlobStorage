070c389f683cf38d9e7bb81679b2280fe2733127
damienbod <damien_bod@hotmail.com>
2021-02-05 10:52:33 +0100

    init project

---
+++ AspNetCoreAzureStorage/Pages/CallApi.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAzureStorage.Pages
+{
+    public class CallApiModel : PageModel
+    {
+        private readonly AzureStorageService _azureStorageService;
+
+        public JArray DataFromApi { get; set; }
+        public CallApiModel(AzureStorageService azureStorageService)
+        {
+            _azureStorageService = azureStorageService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            DataFromApi = await _azureStorageService.GetApiDataAsync();
+        }
+    }
+}
