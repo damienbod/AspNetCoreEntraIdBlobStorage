778d0ac5540f1dc571847be3a05b5a748d6170b6
damienbod <damien_bod@hotmail.com>
2021-02-12 10:55:51 +0100

    Add the GraphApiClientService

---
+++ AspNetCoreAzureStorage/GraphApiClientService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAzureStorage
+{
+    public class GraphApiClientService
+    {
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public GraphApiClientService(GraphServiceClient graphServiceClient)
+        {
+            _graphServiceClient = graphServiceClient;
+        }
+
+        public async Task<User> GetGraphApiUser()
+        {
+            return await _graphServiceClient
+                .Me
+                .Request()
+                //.WithScopes("User.ReadBasic.All", "user.read")
+                .GetAsync()
+                .ConfigureAwait(false);
+        }
+    }
+}
