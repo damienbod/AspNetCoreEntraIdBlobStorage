47f81d92a950fcc7a0d560da758490f42937e553
damienbod <damien_bod@hotmail.com>
2021-02-19 16:22:52 +0100

    Adding Azure AD groups to validate the Storage access

--- AspNetCoreAzureStorageGroups/Authz/StorageBlobDataContributorRoleHandler.cs
+++ AspNetCoreAzureStorageGroups/Authz/StorageBlobDataContributorRoleHandler.cs
@@ -17,9 +17,15 @@ namespace AspNetCoreAzureStorageGroups
             if (requirement == null)
                 throw new ArgumentNullException(nameof(requirement));
 
-            // TODO add auth using group
+            //  "6705345e-c37e-4f7a-b2d9-e2f43e029524" // StorageContributorsAzureADfiles
 
-            context.Succeed(requirement);
+            var spIdUserGroup = context.User.Claims.FirstOrDefault(t => t.Type == "groups" && 
+            t.Value == "6705345e-c37e-4f7a-b2d9-e2f43e029524");
+
+            if(spIdUserGroup != null)
+            {
+                context.Succeed(requirement);
+            }
 
             return Task.CompletedTask;
         }
--- AspNetCoreAzureStorageGroups/Authz/StorageBlobDataReaderRoleHandler.cs
+++ AspNetCoreAzureStorageGroups/Authz/StorageBlobDataReaderRoleHandler.cs
@@ -18,8 +18,17 @@ namespace AspNetCoreAzureStorageGroups
             if (requirement == null)
                 throw new ArgumentNullException(nameof(requirement));
 
-            // TODO add auth using group
+            //  "6705345e-c37e-4f7a-b2d9-e2f43e029524" // StorageContributorsAzureADfiles
+            //  "a7bd0f89-ff92-48af-b14b-c4b4a77ea6cf" // StorageReadersAzureADfiles
 
+            var spIdUserGroup = context.User.Claims.FirstOrDefault(t => t.Type == "groups" &&
+              (t.Value == "6705345e-c37e-4f7a-b2d9-e2f43e029524" ||
+              t.Value == "a7bd0f89-ff92-48af-b14b-c4b4a77ea6cf"));
+
+            if (spIdUserGroup != null)
+            {
+                context.Succeed(requirement);
+            }
             context.Succeed(requirement);
 
             return Task.CompletedTask;
