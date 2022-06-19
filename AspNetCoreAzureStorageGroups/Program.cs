9beda3cce533f6d905b03b88156bb3089bf7f7da
damienbod <damien_bod@hotmail.com>
2022-06-19 16:37:48 +0200

    .NET 6 styles

--- AspNetCoreAzureStorageGroups/Program.cs
+++ AspNetCoreAzureStorageGroups/Program.cs
@@ -1,15 +1,11 @@
 using AspNetCoreAzureStorageGroups;
 using AspNetCoreAzureStorageGroups.FilesProvider.AzureStorageAccess;
 using AspNetCoreAzureStorageGroups.FilesProvider.SqlDataAccess;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
