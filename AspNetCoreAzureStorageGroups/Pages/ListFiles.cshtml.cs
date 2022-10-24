149cd0251571e2aee25ad00d2676810114637666
damienbod <damien_bod@hotmail.com>
2022-10-24 10:40:53 +0200

    clean up

--- AspNetCoreAzureStorageGroups/Pages/ListFiles.cshtml.cs
+++ AspNetCoreAzureStorageGroups/Pages/ListFiles.cshtml.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using AspNetCoreAzureStorageGroups.FilesProvider.AzureStorageAccess;
 using AspNetCoreAzureStorageGroups.FilesProvider.SqlDataAccess;
 using AspNetCoreAzureStorageGroups.FilesProvider.ViewModels;
@@ -8,41 +6,40 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Identity.Web;
 
-namespace AspNetCoreAzureStorageGroups.Pages
+namespace AspNetCoreAzureStorageGroups.Pages;
+
+[Authorize(Policy = "StorageBlobDataReaderPolicy")]
+[AuthorizeForScopes(Scopes = new string[] { "https://storage.azure.com/user_impersonation" })]
+public class ListFilesModel : PageModel
 {
-    [Authorize(Policy = "StorageBlobDataReaderPolicy")]
-    [AuthorizeForScopes(Scopes = new string[] { "https://storage.azure.com/user_impersonation" })]
-    public class ListFilesModel : PageModel
+    private readonly AzureStorageProvider _azureStorageService;
+    private readonly FileDescriptionProvider _fileDescriptionProvider;
+
+    [BindProperty]
+    public IEnumerable<FileDescriptionDto> FileDescriptions { get; set; } 
+        = new List<FileDescriptionDto>();
+
+    [BindProperty]
+    public string? FileName { get; set; }
+
+    public ListFilesModel(AzureStorageProvider azureStorageService,
+        FileDescriptionProvider fileDescriptionProvider)
     {
-        private readonly AzureStorageProvider _azureStorageService;
-        private readonly FileDescriptionProvider _fileDescriptionProvider;
-
-        [BindProperty]
-        public IEnumerable<FileDescriptionDto> FileDescriptions { get; set; } 
-            = new List<FileDescriptionDto>();
-
-        [BindProperty]
-        public string? FileName { get; set; }
-
-        public ListFilesModel(AzureStorageProvider azureStorageService,
-            FileDescriptionProvider fileDescriptionProvider)
-        {
-            _azureStorageService = azureStorageService;
-            _fileDescriptionProvider = fileDescriptionProvider;
-        }
-
-        public void OnGetAsync()
-        {
-            // should only return this dat if authz is good.
-            FileDescriptions = _fileDescriptionProvider.GetAllFiles();
-        }
-
-        public async Task<ActionResult> OnGetDownloadFile(string fileName)
-        {
-            var file = await _azureStorageService.DownloadFile(fileName);
-
-            return File(file.Value.Content, file.Value.ContentType, fileName);
-        }
-
+        _azureStorageService = azureStorageService;
+        _fileDescriptionProvider = fileDescriptionProvider;
     }
+
+    public void OnGetAsync()
+    {
+        // should only return this dat if authz is good.
+        FileDescriptions = _fileDescriptionProvider.GetAllFiles();
+    }
+
+    public async Task<ActionResult> OnGetDownloadFile(string fileName)
+    {
+        var file = await _azureStorageService.DownloadFile(fileName);
+
+        return File(file.Value.Content, file.Value.ContentType, fileName);
+    }
+
 }
