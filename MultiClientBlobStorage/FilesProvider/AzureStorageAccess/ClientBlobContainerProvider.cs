dd6721d37b64615685718f7e0b15b62844158654
damienbod <damien_bod@hotmail.com>
2024-02-10 13:14:48 +0100

    use the client

--- MultiClientBlobStorage/FilesProvider/AzureStorageAccess/ClientBlobContainerProvider.cs
+++ MultiClientBlobStorage/FilesProvider/AzureStorageAccess/ClientBlobContainerProvider.cs
@@ -1,4 +1,6 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace MultiClientBlobStorage.FilesProvider.AzureStorageAccess;
 
@@ -18,8 +20,10 @@ public class ClientBlobContainerProvider
     {
         try
         {
+
             // 1. Create new security group for client users
             // 2. Create new Blob container
+            CreateContainer(clientName);
             // 3. RBAC security group Blob data read
             // NOTE blob write is configured on root 
 
@@ -32,14 +36,42 @@ public class ClientBlobContainerProvider
         }
     }
 
-    private async Task<string> PersistFileToAzureStorage(string clientName)
+    private BlobContainerClient CreateContainer(string name)
     {
-        var storage = _configuration.GetValue<string>("AzureStorage:StorageAndContainerName");
-        var fileFullName = $"{storage}/{clientName}";
-        var blobUri = new Uri(fileFullName);
+        try
+        {
+            string containerName = $"container-{Guid.NewGuid()}".ToLower();
+            var storage = _configuration.GetValue<string>("AzureStorage:StorageAndContainerName");
+            var credential = _clientSecretCredentialProvider.GetClientSecretCredential();
 
-        var blobClient = new BlobClient(blobUri, _clientSecretCredentialProvider.GetClientSecretCredential());
+            if(storage != null && credential != null)
+            {
+                var blobServiceClient = new BlobServiceClient(new Uri(storage), credential);
+                var metadata = new Dictionary<string, string?>
+                {
+                    { "name", name },
+                };
 
-        return $"{clientName}: Azure Blob Storage Container created";
+                // Create the root container or handle the exception if it already exists
+                BlobContainerClient container = blobServiceClient.CreateBlobContainer(containerName, 
+                    PublicAccessType.BlobContainer,
+                    metadata);
+
+                if (container.Exists())
+                {
+                    Console.WriteLine($"Created container: {name} {container.Name}");
+                }
+
+                return container;
+            }
+
+            throw new Exception($"Could not create container: {name}");
+        }
+        catch (RequestFailedException e)
+        {
+            Console.WriteLine("HTTP error code {0}: {1}", e.Status, e.ErrorCode);
+            Console.WriteLine(e.Message);
+            throw;
+        }
     }
 }
