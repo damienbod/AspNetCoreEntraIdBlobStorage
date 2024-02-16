f6894a33a6d609441b1f2b20a2bbf6e732747ee1
damienbod <damien_bod@hotmail.com>
2024-02-16 07:58:57 +0100

    Add logs

--- MultiClientBlobStorage/Providers/AzureMgmtClientService.cs
+++ MultiClientBlobStorage/Providers/AzureMgmtClientService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
 
 namespace AzureMgmtClientCrendentials;
 
@@ -7,14 +8,17 @@ public class AzureMgmtClientService
     private readonly AzureMgmtClientCredentialService _azureMgmtClientCredentialService;
     private readonly IHttpClientFactory _clientFactory;
     private readonly IConfiguration _configuration;
+    private readonly ILogger<AzureMgmtClientService> _logger;
 
     public AzureMgmtClientService(AzureMgmtClientCredentialService azureMgmtClientCredentialService,
         IHttpClientFactory clientFactory,
-        IConfiguration configuration)
+        IConfiguration configuration,
+        ILogger<AzureMgmtClientService> logger)
     {
         _azureMgmtClientCredentialService = azureMgmtClientCredentialService;
         _clientFactory = clientFactory;
         _configuration = configuration;
+        _logger = logger;
     }
 
     /// <summary>
@@ -47,44 +51,52 @@ public class AzureMgmtClientService
 
         var PayloadRoleAssignment = new PayloadRoleAssignment
         {
-            properties = new properties
+            Properties = new Properties
             {
-                roleDefinitionId = roleDefinitionId,
-                principalId = servicePrincipalId,
-                principalType = "Group"
+                RoleDefinitionId = roleDefinitionId,
+                PrincipalId = servicePrincipalId,
+                PrincipalType = "Group"
             }
         };
 
         // view containers
-        var getRe = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}/blobServices/default/containers?api-version=2023-01-01";
-        var response = await client.GetAsync(getRe);
-        var test = await response.Content.ReadAsStringAsync();
+        //var getRe = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}/blobServices/default/containers?api-version=2023-01-01";
+        //var response = await client.GetAsync(getRe);
+        //var test = await response.Content.ReadAsStringAsync();
 
-        response = await client.PutAsJsonAsync<PayloadRoleAssignment>(url, PayloadRoleAssignment);
+        var response = await client.PutAsJsonAsync<PayloadRoleAssignment>(url, PayloadRoleAssignment);
         if (response.IsSuccessStatusCode)
         {
             var responseContent = await response.Content.ReadAsStringAsync();
+            _logger.LogInformation("Created RBAC for read group {blobContainerName} {responseContent}", blobContainerName, responseContent);
             return;
         }
 
-        throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
+        var responseError = await response.Content.ReadAsStringAsync();
+        _logger.LogCritical("Created RBAC for read group {blobContainerName} {responseError}", blobContainerName, responseError);
+        throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}, {responseError}");
     }
 
-    public class PayloadRoleAssignment
+    private class PayloadRoleAssignment
     {
-        public properties properties { get; set; } = new();
+        [JsonPropertyName("properties")]
+        public Properties Properties { get; set; } = new();
     }
 
     /// <summary>
-    /// "properties": {
-    ///    "roleDefinitionId": "subscriptions/SUBSCRIPTION_ID/resourcegroups/RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/STORAGE_ACCOUNT_NAME/providers/Microsoft.Authorization/roleDefinitions/ROLE_ID",
-    ///    "principalId": "SP_ID"
-    ///}
+    ///     "properties": {
+    ///     "roleDefinitionId":
+    ///     "subscriptions/SUBSCRIPTION_ID/resourcegroups/RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/STORAGE_ACCOUNT_NAME/providers/Microsoft.Authorization/roleDefinitions/ROLE_ID",
+    ///     "principalId": "SP_ID"
+    ///     }
     /// </summary>
-    public class properties
+    private class Properties
     {
-        public string roleDefinitionId { get; set; } = string.Empty;
-        public string principalId { get; set; } = string.Empty;
-        public string principalType { get; set; } = "Group";
+        [JsonPropertyName("roleDefinitionId")]
+        public string RoleDefinitionId { get; set; } = string.Empty;
+        [JsonPropertyName("principalId")]
+        public string PrincipalId { get; set; } = string.Empty;
+        [JsonPropertyName("principalType")]
+        public string PrincipalType { get; set; } = "Group";
     }
 }
