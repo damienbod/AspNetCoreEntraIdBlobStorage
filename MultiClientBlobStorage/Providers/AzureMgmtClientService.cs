28d0ce9dc7ba116e2425a9477e89c75d45009316
damienbod <damien_bod@hotmail.com>
2024-02-16 07:24:11 +0100

    test

--- MultiClientBlobStorage/Providers/AzureMgmtClientService.cs
+++ MultiClientBlobStorage/Providers/AzureMgmtClientService.cs
@@ -20,6 +20,8 @@ public class AzureMgmtClientService
     /// <summary>
     /// Storage Blob Data Reader: ID: 2a2b9908-6ea1-4ae2-8e65-a410df84e7d1
     /// Role assignment required for application in Azure on resource group
+    /// https://learn.microsoft.com/en-us/rest/api/authorization/role-assignments/create-by-id?view=rest-authorization-2022-04-01&tabs=HTTP
+    /// https://learn.microsoft.com/en-us/azure/role-based-access-control/role-assignments-rest
     /// </summary>
     public async Task StorageBlobDataReaderRoleAssignment(string groupId, string storageAccountName, string blobContainerName)
     {
@@ -31,7 +33,8 @@ public class AzureMgmtClientService
         // the resource group name
         var resourceGroupName = _configuration["AzureMgmt:ResourceGroupName"];
 
-        var url = $"https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}/providers/Microsoft.Authorization/roleAssignments/{roleId}?api-version=2022-04-01";
+        var objectId = $"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}/blobServices/default/containers/{blobContainerName}";     
+        var url = $"https://management.azure.com{objectId}/providers/Microsoft.Authorization/roleAssignments/{roleId}?api-version=2022-04-01";
 
         var client = _clientFactory.CreateClient();
         var accessToken = await _azureMgmtClientCredentialService.GetAccessToken();
@@ -39,21 +42,24 @@ public class AzureMgmtClientService
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-        var roleDefinitionId = $"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}/blobServices/default/containers/{blobContainerName}/providers/Microsoft.Authorization/roleDefinitions/{roleId}";
+        var roleDefinitionId = $"{objectId}/providers/Microsoft.Authorization/roleDefinitions/{roleId}";
+
         var PayloadRoleAssignment = new PayloadRoleAssignment
         {
             properties = new properties
             {
                 roleDefinitionId = roleDefinitionId,
-                principalId = servicePrincipalId
+                principalId = servicePrincipalId,
+                principalType = "Group"
             }
         };
 
         // view containers
-        //var getRe = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}/blobServices/default/containers?api-version=2023-01-01";
-        //var response = await client.GetAsync(getRe);
+        var getRe = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}/blobServices/default/containers?api-version=2023-01-01";
+        var response = await client.GetAsync(getRe);
+        var test = await response.Content.ReadAsStringAsync();
 
-        var response = await client.PutAsJsonAsync<PayloadRoleAssignment>(url, PayloadRoleAssignment);
+        response = await client.PutAsJsonAsync<PayloadRoleAssignment>(url, PayloadRoleAssignment);
         if (response.IsSuccessStatusCode)
         {
             var responseContent = await response.Content.ReadAsStringAsync();
@@ -78,6 +84,6 @@ public class AzureMgmtClientService
     {
         public string roleDefinitionId { get; set; } = string.Empty;
         public string principalId { get; set; } = string.Empty;
-
+        public string principalType { get; set; } = "Group";
     }
 }
